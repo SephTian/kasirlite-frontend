b8b60ea8addb73c4d83f40fc6687b6ccdb26ebcc
JoJoseph <sephtianyt@gmail.com>
2024-11-23 02:29:55 +0700

    Refactor: added option on the function params

--- src/lib/services/api.ts
+++ src/lib/services/api.ts
@@ -2,14 +2,14 @@ import fetchAxios from '@/utils/axios';
 import { LoginFormData } from '../types';
 
 const api = (() => {
-  async function login({ email, password }: LoginFormData) {
+  async function login({ email, password, ...option }: LoginFormData) {
     const API_DATA = {
       email,
       password,
     };
     const {
       data: { data, status, message },
-    } = await fetchAxios.post(`/api/auth/login`, API_DATA);
+    } = await fetchAxios.post(`/api/auth/login`, API_DATA, { ...option });
 
     if (status !== 'ok') {
       throw new Error(message);
