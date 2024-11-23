24d14ad90bab19cf60c1e1a94e5fb08a652c9ddc
JoJoseph <sephtianyt@gmail.com>
2024-11-23 18:57:17 +0700

    Feat: Poppins font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from 'next';
 import '@/styles/globals.css';
 import Providers from '@/components/layouts/Providers';
+import { Poppins } from 'next/font/google';
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'], // Sesuaikan dengan kebutuhan
+});
 
 export const metadata: Metadata = {
   title: 'KasirLite',
@@ -14,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>
+      <body className={poppins.className}>
         <Providers>{children}</Providers>
       </body>
     </html>
