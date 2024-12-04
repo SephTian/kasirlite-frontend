c124650ca990b4f20982d3c7aea9054973a6c161
JoJoseph <sephtianyt@gmail.com>
2024-12-04 23:24:17 +0700

    style: some detail

--- src/components/order-cart/menu-card.tsx
+++ src/components/order-cart/menu-card.tsx
@@ -13,17 +13,13 @@ export default function MenuCard({ id, image, name, price, discount, menuType, m
 
   return (
     <div className={`transition-transform relative transform duration-100 rounded-md bg-gray-200 p-1 ${disabled ? 'cursor-not-allowed ' : 'cursor-pointer hover:scale-105'}`} onClick={handleClick}>
-      <div className="space-y-1">
-        <div className="w-full rounded-sm aspect-video bg-gray-300">{/* TODO: PASANG IMAGE */}</div>
-        <div className="">
-          <p className="font-bold truncate text-xs">
-            {name} - {id} - {image}
-          </p>
-          <p className="font-medium text-xs line-through text-gray-500">Rp.{price}</p>
-          <p className="font-medium text-xs">Rp.{price - discount}</p>
-        </div>
+      <div className="w-full rounded-sm aspect-video bg-gray-300">{/* TODO: PASANG IMAGE */}</div>
+      <div className="">
+        <p className="font-bold truncate text-xs my-2">
+          {name} - {id} - {image}
+        </p>
+        <p className={`font-medium text-xs`}>Rp.{price - discount}</p>
       </div>
-
       {disabled && <div className="bg-white w-full h-full absolute top-0 left-0 opacity-50"></div>}
     </div>
   );
