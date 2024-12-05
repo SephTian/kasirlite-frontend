1b2056a3637e971f321578204474567c97ad44fc
JoJoseph <sephtianyt@gmail.com>
2024-12-05 22:29:10 +0800

    Refactor: make handleEditModal props optional

--- src/components/order-cart/receipt-table.tsx
+++ src/components/order-cart/receipt-table.tsx
@@ -3,12 +3,12 @@ import React from 'react';
 
 type Props = {
   cart: MenuCart[];
-  handleEditModal: (idx: number) => void;
+  handleEditModal?: (idx: number) => void;
 };
 
 export default function ReceiptTable({ cart, handleEditModal }: Props) {
   return (
-    <div className="h-[350px] overflow-y-auto border border-gray-500 rounded-sm">
+    <div className="h-full overflow-y-auto border border-gray-500 rounded-sm">
       <table className="table-auto w-full text-left ">
         <thead className="sticky top-0 text-xs font-medium text-gray-50 bg-gray-500">
           <tr className="w-full">
@@ -20,7 +20,7 @@ export default function ReceiptTable({ cart, handleEditModal }: Props) {
         <tbody className="text-xs text-customBlack">
           {cart.map((item, idx) => {
             return (
-              <tr key={idx} onClick={() => handleEditModal(idx)} className="odd:bg-slate-300 even:bg-slate-200 w-full cursor-pointer">
+              <tr key={idx} onClick={() => handleEditModal?.(idx)} className="odd:bg-slate-300 even:bg-slate-200 w-full cursor-pointer">
                 <td className="px-2 py-4">{item.quantity}</td>
                 <td className="px-2 py-4">{item.menu.name}</td>
                 <td className="px-2 py-4 font-semibold">Rp.{(item.menu.price - item.menu.discount) * item.quantity}</td>
