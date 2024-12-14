7a215a047583b8478aa6b19d2d90a548d241b926
JoJoseph <sephtianyt@gmail.com>
2024-12-14 16:47:51 +0800

    Refactor: changing some prop type

--- src/components/order-cart/menu-card.tsx
+++ src/components/order-cart/menu-card.tsx
@@ -1,24 +1,25 @@
-import { Menu } from '@/lib/types';
+import { Menu, MenuCart } from '@/lib/types';
+import { formatRupiah } from '@/utils';
 import React from 'react';
-type Props = Menu & {
-  onClick?: (param: Omit<Menu, 'isAdditional' | 'disabled'>) => void;
+type Props = Omit<Menu, 'menuType' | 'menuTypeId'> & {
+  onAddCart?: (param: MenuCart['menu']) => void;
 };
 
-export default function MenuCard({ id, image, name, price, discount, menuType, menuTypeId, disabled, onClick }: Props) {
+export default function MenuCard({ id, image, name, price, discount, disabled, onAddCart }: Props) {
   const handleClick = () => {
     if (!disabled) {
-      onClick?.({ id, image, name, menuType, menuTypeId, price, discount });
+      onAddCart?.({ id, name, price, discount });
     }
   };
 
   return (
     <div className={`transition-transform relative transform duration-100 rounded-md bg-gray-200 p-1 ${disabled ? 'cursor-not-allowed ' : 'cursor-pointer hover:scale-105'}`} onClick={handleClick}>
-      <div className="w-full rounded-sm aspect-video bg-gray-300">{/* TODO: PASANG IMAGE */}</div>
+      <div className="w-full rounded-sm aspect-video bg-gray-300">
+        {/* TODO: PASANG IMAGE */} {image}
+      </div>
       <div className="">
-        <p className="font-bold truncate text-xs my-2">
-          {name} - {id} - {image}
-        </p>
-        <p className={`font-medium text-xs`}>Rp.{price - discount}</p>
+        <p className="font-bold truncate text-xs my-2">{name}</p>
+        <p className={`font-medium text-xs`}>{formatRupiah(price - discount)}</p>
       </div>
       {disabled && <div className="bg-white w-full h-full absolute top-0 left-0 opacity-50"></div>}
     </div>
