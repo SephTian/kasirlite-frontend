ed58bd63834b3f60c6aeb91e57f91bd9f38b914d
JoJoseph <sephtianyt@gmail.com>
2025-01-03 16:30:54 +0800

    Refactor: make modal more styleable

--- src/components/custom-ui/modal-wrapper.tsx
+++ src/components/custom-ui/modal-wrapper.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { ReactNode } from 'react';
+import { ReactNode, useEffect, useRef } from 'react';
 
 type Props = {
   isOpen: boolean;
@@ -8,14 +8,34 @@ type Props = {
 };
 
 function ModalWrapper({ isOpen, closeModal, children }: Props) {
+  const modalRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const handleClickOutsideModal = (e: MouseEvent) => {
+      if (modalRef.current && !modalRef.current.contains(e.target as Node)) {
+        closeModal();
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener('click', handleClickOutsideModal);
+    }
+
+    return () => {
+      document.removeEventListener('click', handleClickOutsideModal);
+    };
+  }, [isOpen, closeModal]);
+
   if (!isOpen) {
     return null;
   }
 
   return (
-    <div className="fixed top-0 left-0 z-[1000] w-screen h-screen flex items-center justify-center">
-      <div className="w-full h-full bg-black opacity-40" onClick={closeModal}></div>
-      <div className="absolute max-h-[calc(100vh-4rem)] overflow-y-scroll bg-white rounded-sm p-3">{children}</div>
+    <div className="fixed top-0 left-0 z-[1000] w-screen h-screen flex items-center justify-center overflow-y-scroll overflow-hidden bg-black/50 p-6">
+      {/* <div className="absolute -z-10 w-full h-full flex-grow bg-black opacity-40" onClick={closeModal}></div> */}
+      <div ref={modalRef} className="m-auto h-fit bg-white rounded-sm p-3">
+        {children}
+      </div>
     </div>
   );
 }
