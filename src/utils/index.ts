f9443ce0a148382982b64d04db934688d71f9193
JoJoseph <sephtianyt@gmail.com>
2025-01-09 18:39:29 +0800

    Feat: new function to format and unformat price

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,21 @@
 export function formatRupiah(amount: number): string {
   return `Rp${amount.toLocaleString('id-ID')}`;
 }
+
+export function formatPrice(unformatedPrice: number): string {
+  let stringPrice = unformatedPrice.toString();
+  let isMinus = false;
+  if (stringPrice.charAt(0) === '-') {
+    isMinus = true;
+  }
+  stringPrice = stringPrice.replace(/\D/g, ''); // Delete all char not number
+  stringPrice = stringPrice.replace(/\B(?=(\d{3})+(?!\d))/g, '.'); // Make number to be currency
+  stringPrice = isMinus ? '-' + stringPrice : stringPrice;
+  return stringPrice;
+}
+
+export function unformatPrice(formatedPrice: string): number {
+  const result = formatedPrice.replace(/[^0-9]/g, '');
+
+  return parseInt(result);
+}
