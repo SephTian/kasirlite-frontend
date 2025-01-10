16422233cec3ce6293d11a2ef90cee9a16465d83
JoJoseph <sephtianyt@gmail.com>
2025-01-10 21:23:38 +0800

    Refactor: removing calculate accesToken expire time on middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { getToken } from 'next-auth/jwt';
-import { verifyJwt } from './utils/jwt';
 
 const protectedURL = ['/', '/setting'];
 
@@ -12,22 +11,19 @@ export async function middleware(request: NextRequest) {
     secret: process.env.SECRET_KEY,
   });
 
-  // Pengecekan token untuk autentikasi, jika ridak ada token (nextauth atau accesstoken), maka akan tebuang ke login page
+  // Pengecekan token untuk autentikasi, jika tidak ada token (nextauth atau accesstoken), maka akan tebuang ke login page
   if (protectedURL.includes(request.nextUrl.pathname) && !token) {
     return NextResponse.redirect(new URL('/login', request.url));
   }
 
   if (token && token.accessToken) {
-    const decodedJWT = await verifyJwt(token?.accessToken as string, process.env.SECRET_KEY || '');
-    const tokenExpInMs = (decodedJWT.exp as number) * 1000;
-
-    // Pengecekan sudah login tapi expire
-    if (protectedURL.includes(request.nextUrl.pathname) && Date.now() > tokenExpInMs) {
+    // Pengecekan sudah login tapi expire -> accessToken yang didapatkan dari api sudah expire
+    if (protectedURL.includes(request.nextUrl.pathname) && Date.now() > (token.accessTokenExpires as number)) {
       return NextResponse.redirect(new URL('/login', request.url));
     }
 
     // jika sudah login dan access token belum expire, maka dilarang untuk ke halaman login
-    if (request.nextUrl.pathname === '/login' && Date.now() < tokenExpInMs) {
+    if (request.nextUrl.pathname === '/login' && Date.now() < (token.accessTokenExpires as number)) {
       return NextResponse.redirect(new URL('/', request.url));
     }
   }
