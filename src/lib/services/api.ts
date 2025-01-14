8cae31c3e8dee814ce16680feb84b42dbc4f5eba
JoJoseph <sephtianyt@gmail.com>
2025-01-14 18:13:53 +0800

    Refactor: make all api function has try catch

--- src/lib/services/api.ts
+++ src/lib/services/api.ts
@@ -1,76 +1,101 @@
 import fetchAxios from '@/utils/axios';
 import { LoginFormData, TransactionFormData } from '../types';
+import { AxiosError } from 'axios';
 
 const api = (() => {
   async function login({ email, password, ...option }: LoginFormData) {
-    const API_DATA = {
-      email,
-      password,
-    };
-    const {
-      data: { data, status, message },
-    } = await fetchAxios.post(`/api/auth/login`, API_DATA, { ...option });
+    try {
+      const API_DATA = {
+        email,
+        password,
+      };
+      const {
+        data: { data, status, message },
+      } = await fetchAxios.post(`/api/auth/login`, API_DATA, { ...option });
 
-    if (status !== 'ok') {
-      throw new Error(message);
+      if (status !== 'ok') {
+        throw new Error(message);
+      }
+
+      const { user } = data;
+
+      return user;
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        throw new Error(error.response?.data?.message || 'error');
+      }
     }
-
-    const { user } = data;
-
-    return user;
   }
 
   async function getMenus({ ...option }) {
-    const {
-      data: { data, status, message },
-    } = await fetchAxios.get(`/api/menus`, { ...option });
+    try {
+      const {
+        data: { data, status, message },
+      } = await fetchAxios.get(`/api/menus`, { ...option });
 
-    if (status !== 'ok') {
-      throw new Error(message);
+      if (status !== 'ok') {
+        throw new Error(message);
+      }
+
+      const { menus } = data;
+
+      return menus;
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        throw new Error(error.response?.data?.message || 'error');
+      }
     }
-
-    const { menus } = data;
-
-    return menus;
   }
 
   async function getMenuCategories({ ...option }) {
-    const {
-      data: { data, status, message },
-    } = await fetchAxios.get(`/api/menu-categories`, { ...option });
+    try {
+      const {
+        data: { data, status, message },
+      } = await fetchAxios.get(`/api/menu-categories`, { ...option });
 
-    if (status !== 'ok') {
-      throw new Error(message);
+      if (status !== 'ok') {
+        throw new Error(message);
+      }
+
+      const { menuCategories } = data;
+
+      return menuCategories;
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        throw new Error(error.response?.data?.message || 'error');
+      }
     }
-
-    const { menuCategories } = data;
-
-    return menuCategories;
   }
 
   async function addTransaction({ cart, totalPrice, discount, customerName, note, type, paymentType, paymentKind, ...option }: TransactionFormData) {
-    const {
-      data: { data, status, message },
-    } = await fetchAxios.post(
-      `/api/transactions`,
-      {
-        cart: cart,
-        totalPrice: totalPrice,
-        discount: discount,
-        customerName: customerName,
-        note: note ?? '',
-        type: type,
-        paymentType: paymentType || null,
-        paymentKind: paymentKind,
-      },
-      { ...option }
-    );
+    try {
+      const {
+        data: { data, status, message },
+      } = await fetchAxios.post(
+        `/api/transactions`,
+        {
+          cart: cart,
+          totalPrice: totalPrice,
+          discount: discount,
+          customerName: customerName,
+          note: note ?? '',
+          type: type,
+          paymentType: paymentType || null,
+          paymentKind: paymentKind,
+        },
+        { ...option }
+      );
 
-    if (status !== 'ok') {
-      throw new Error(message);
+      if (status !== 'ok') {
+        throw new Error(message);
+      }
+
+      console.log(data);
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        throw new Error(error.response?.data?.message || 'error');
+      }
     }
-
-    console.log(data, status, message);
 
     // return Order;
   }
