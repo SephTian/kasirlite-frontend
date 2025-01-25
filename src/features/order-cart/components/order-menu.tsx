9f012dd951a7407e02e7f411c675267b0e8c8bb2
JoJoseph <sephtianyt@gmail.com>
2025-01-25 16:30:51 +0800

    Refactor: changing search filter componentn

--- src/features/order-cart/components/order-menu.tsx
+++ src/features/order-cart/components/order-menu.tsx
@@ -3,11 +3,11 @@
 import MenuCategoryFilter from './menu-category-filter';
 import { MenuCart } from '@/lib/types';
 import { useSearchParams } from 'next/navigation';
-import MenuQueryFilter from './menu-query-filter';
 import { useQuery } from '@tanstack/react-query';
 import api from '@/lib/services/api';
 import { getSession } from 'next-auth/react';
 import MenuList from './menu-list';
+import DebounceInput from '@/components/custom-ui/debounce-input';
 
 type Props = {
   onOpenAddCartModal: (param: MenuCart['menu']) => void;
@@ -58,7 +58,7 @@ export default function OrderMenu({ onOpenAddCartModal }: Props) {
 
   return (
     <>
-      <MenuQueryFilter selectedKeyword={menuKeywordParams} />
+      <DebounceInput classname="w-full" placeholder="Cari menu.." debounceTime={500} searchParamName="keyword" />
       {menuCategoryData ? <MenuCategoryFilter menuCategories={menuCategoryData} selectedCategory={menuCategoryParams} /> : <p>Silahkan muat ulang halaman</p>}
       <h1 className="font-semibold">Daftar makanan:</h1>
       <div className="w-full p-2 min-h-0 flex-auto overflow-y-scroll rounded-lg bg-slate-100 shadow-inner">
