b11ce310942234755c7b1c35517efd45d4bd56b4
JoJoseph <sephtianyt@gmail.com>
2025-01-25 19:05:32 +0800

    Fix: search is not working

--- src/features/order-cart/components/order-menu.tsx
+++ src/features/order-cart/components/order-menu.tsx
@@ -2,12 +2,13 @@
 
 import MenuCategoryFilter from './menu-category-filter';
 import { MenuCart } from '@/lib/types';
-import { useSearchParams } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 import { useQuery } from '@tanstack/react-query';
 import api from '@/lib/services/api';
 import { getSession } from 'next-auth/react';
 import MenuList from './menu-list';
 import DebounceInput from '@/components/custom-ui/debounce-input';
+import { useCallback } from 'react';
 
 type Props = {
   onOpenAddCartModal: (param: MenuCart['menu']) => void;
@@ -15,6 +16,7 @@ type Props = {
 
 export default function OrderMenu({ onOpenAddCartModal }: Props) {
   const searchParams = useSearchParams();
+  const router = useRouter();
   const menuKeywordParams = searchParams.get('keyword')?.toLowerCase() || '';
   const menuCategoryParams = searchParams.get('category')?.toLowerCase() || '';
 
@@ -56,9 +58,18 @@ export default function OrderMenu({ onOpenAddCartModal }: Props) {
     refetchOnReconnect: false,
   });
 
+  const handleSearchChange = useCallback(
+    (keyword: string) => {
+      const params = new URLSearchParams(searchParams.toString());
+      params.set('keyword', keyword);
+      router.replace(`?${params.toString()}`, { scroll: false });
+    },
+    [router, searchParams]
+  );
+
   return (
     <>
-      <DebounceInput classname="w-full" placeholder="Cari menu.." debounceTime={500} searchParamName="keyword" />
+      <DebounceInput classname="w-full" placeholder="Cari menu.." debounceTime={500} searchParamName="keyword" onChange={handleSearchChange} />
       {menuCategoryData ? <MenuCategoryFilter menuCategories={menuCategoryData} selectedCategory={menuCategoryParams} /> : <p>Silahkan muat ulang halaman</p>}
       <h1 className="font-semibold">Daftar makanan:</h1>
       <div className="w-full p-2 min-h-0 flex-auto overflow-y-scroll rounded-lg bg-slate-100 shadow-inner">
